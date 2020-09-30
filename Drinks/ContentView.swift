f14605362391ea52347219c9efc9c7fa5ccf0aa7
RAM111111 <32421153+RAM111111@users.noreply.github.com>
2020-09-30 03:04:24 +0300

    Initial Commit

---
+++ Drinks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Drinks
+//
+//  Created by ر on 13/02/1442 AH.
+//  Copyright © 1442 r. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
